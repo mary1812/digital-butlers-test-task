9dd30b820770d719e96ab149fb701b2103cd06ef
Mary <mariyapiligrim3@gmail.com>
2022-05-08 23:51:45 +0300

    feat: add header & main section components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Header from './components/Header';
+import MainSection from './components/MainSection';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header/>
+      <MainSection/>
     </div>
   );
 }
