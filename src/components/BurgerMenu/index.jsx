e047dae986aa240260223f454b01b72ec910555a
Mary <mariyapiligrim3@gmail.com>
2022-05-08 20:12:12 +0300

    feat: add burger menu component

---
+++ src/components/BurgerMenu/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import burger from './../../assets/img/burger.png'
+
+const BurgerMenu = () => {
+  return (
+    <div>
+      <img src={burger} alt="burger"></img>
+    </div>
+  );
+}
+
+export default BurgerMenu;
