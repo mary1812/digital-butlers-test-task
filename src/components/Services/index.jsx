829122afc136aa6b0fb5a092b75930f20ff22a69
Mary <mariyapiligrim3@gmail.com>
2022-05-10 00:24:39 +0300

    feat: add services component

---
+++ src/components/Services/index.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import "./index.css";
+import Cards from "../Cards";
+import progressBar from "./../../assets/img/progressBar.png";
+import card1 from "./../../assets/img/card1.png";
+import card2 from "./../../assets/img/card2.png";
+import card3 from "./../../assets/img/card3.png";
+import card4 from "./../../assets/img/card4.png";
+
+const Services = () => {
+  const cards = [
+    {
+      imgSrc: card1,
+      header: "Общее обследование",
+      paragraph:
+        "Экспертиза отдельных конструкций, обмерные работы, определение прочности, расчет несущей способности и т.д.",
+      href: "/",
+    },
+    {
+      imgSrc: card2,
+      header: "Тепловизионная съемка",
+      paragraph:
+        "Исследование системы отопления, вентиляции и кондиционирования, а также оценка энергоэффективности здания",
+      href: "/",
+    },
+    {
+      imgSrc: card3,
+      header: "Телеинспекция инженерных сетей",
+      paragraph:
+        "Цветная телевизионная съемка внутренней поверхности любых протяженных объектов до 250 метров",
+      href: "/",
+    },
+    {
+      imgSrc: card4,
+      header: "Общее обследование",
+      paragraph:
+        "Экспертиза отдельных конструкций, обмерные работы, определение прочности, расчет несущей способности и т.д.",
+      href: "/",
+    },
+  ];
+
+  return (
+    <div className="servicesWrapper">
+      <div className="textServices">
+        <h1 className="headerServices">Услуги</h1>
+        <h2 className="mainHeaderServices">
+          Выполняем как комплексное обследование, так и отдельные виды работ
+        </h2>
+      </div>
+      <section className="cardSection">
+        {cards.map((cardDesc) => {
+          return (
+            <Cards
+              imgSrc={cardDesc.imgSrc}
+              header={cardDesc.header}
+              paragraph={cardDesc.paragraph}
+              href={cardDesc.href}
+            />
+          );
+        })}
+      </section>
+      <img className="imgLine" src={progressBar} alt="progress line" />
+    </div>
+  );
+};
+
+export default Services;
