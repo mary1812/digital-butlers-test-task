c8252b3dfa19a78028c9a663b79fa42fcf4c1ade
Mary <mariyapiligrim3@gmail.com>
2022-05-10 00:24:59 +0300

    feat: add cards component

---
+++ src/components/Cards/index.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import './index.css'
+import pointer from "./../../assets/img/pointer.png";
+
+const Cards = ({imgSrc, header, paragraph, href}) => {
+  return (
+    <div className="container">
+      <div className="card">
+        <img src={imgSrc} alt="" className="imgHouse" />
+        <h3 className="hCardSection">{header}</h3>
+        <p className="pCardSection">
+         {paragraph}
+        </p>
+        <a href={href} className="aCardSection">
+          Подробнее <img src={pointer} alt="" />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Cards;
