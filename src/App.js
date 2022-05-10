86d3c602c695ab889fccccba4a966800e037161f
Mary <mariyapiligrim3@gmail.com>
2022-05-10 03:09:10 +0300

    feat: add WhyWe and Services components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import Header from './components/Header';
 import MainSection from './components/MainSection';
+import Services from './components/Services';
+import WhyWe from './components/WhyWe';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <MainSection/>
+      <WhyWe/>
+      <Services/>
     </div>
   );
 }
