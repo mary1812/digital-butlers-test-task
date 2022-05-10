f2a8b28b96da047c72faaad975df799810cd84dd
Mary <mariyapiligrim3@gmail.com>
2022-05-10 12:24:10 +0300

    feat: add background change to scroll

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./index.css";
 import mainLogo from "./../../assets/img/mainLogo.png";
 import mail from "./../../assets/img/mail.png";
@@ -6,8 +6,19 @@ import phone from "./../../assets/img/phone.png";
 import BurgerMenu from "./../BurgerMenu";
 
 const Header = () => {
+  const [scroll, setScroll] = useState(0);
+
+  const handleScroll = () => {
+    setScroll(window.scrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <div className="header">
+    <div className={scroll < 50 ? "header" : "header show"}>
       <div className="container">
         <div className="menu">
           <BurgerMenu />
@@ -17,11 +28,22 @@ const Header = () => {
           <div className="email">
             <p className="contactToUs">Пишите нам на email</p>
             <img src={mail} alt="mail logo" className="smallImage"></img>
-            <a href="/" className="linkToEmail">info@diagnistics.by</a>
+            <a href="/" className="linkToEmail">
+              info@diagnistics.by
+            </a>
           </div>
           <div className="phone">
-            <p className="contactToUs"><img src={phone} alt="phone" className="smallImage"></img>Есть вопросы? Звоните!</p>
-            <a href="tel:+375(29)456-45-45" title="click to call" className="phoneNumber">+375 (29) 456-45-45</a>
+            <p className="contactToUs">
+              <img src={phone} alt="phone" className="smallImage"></img>Есть
+              вопросы? Звоните!
+            </p>
+            <a
+              href="tel:+375(29)456-45-45"
+              title="click to call"
+              className="phoneNumber"
+            >
+              +375 (29) 456-45-45
+            </a>
           </div>
         </div>
       </div>
