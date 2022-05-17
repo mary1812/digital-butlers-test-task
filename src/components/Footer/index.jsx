2236117a40963adeb570ce5f17937baa67e9b570
mary1812 <mariyapiligrim3@gmail.com>
2022-05-17 17:51:21 +0300

    refactor: add semantic tags & classnames

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,30 +4,29 @@ import founder from '../../assets/img/founder.png'
 
 const Footer = () => {
   return (
-    <div className="lastSectionWrapper">
-      <div className="footer">
-        <h1 className="hFooter">Мы работаем в комплексе</h1>
-        <p className="pFooter">
+    <section className="lastSectionWrapper">
+      <aside className="lastSectionAside">
+        <h1 className="asideHeader">Мы работаем в комплексе</h1>
+        <p className="asideParagraph">
           чтобы Вы не тратили время на поиск и организацию множества подрядчиков
         </p>
-      </div>
-      <div className="stepOne">
-        <div className="leftstepOne"/>
-        <div className="secondSectionWrapper">
+      </aside>
+      <footer className="lastSection">
+        <div className="footerLeftImage"/>
+        <article className="stepOneArticle">
           <div className="stepOneWrapper">
-            <div className="stepHeader">
-              <span className="spanStepOne">шаг №1. </span>
+            <div className="preparatoryWork">
+              <span className="stepOneSpan">шаг №1. </span>
               <h2 className="stepOneHeader">Подготовительные работы</h2>
             </div>
-            <p className="stepDesc">
+            <p className="stepOneDescription">
               Мы осматриваем здание, делаем замеры и проверяем наличие визуально
               заметных дефектов: трещин, коррозии, повреждения фундамента и т.д.
             </p>
           </div>
           <div className="quoteWrapper">
-          <img className="founderImg" src={founder} alt="" />
+          <img className="founderImg" src={founder} alt="project engineer" />
             <div className="quote">
-            
               <p className="quoteText">
               “Задача этого этапа получить максимально полное представление о конструкции здания, условиях его эксплуатации и возможных слабых местах“
               </p>
@@ -35,11 +34,10 @@ const Footer = () => {
               АЛЕКСЕЙ, обследователь и главный инженер проектов
               </h3>
             </div>
-            
           </div>
-        </div>
-      </div>
-    </div>
+        </article>
+      </footer>
+    </section>
   );
 };
 
